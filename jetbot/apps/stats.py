bdad907b99a0e2fcdcf98b7439762023604b0ce5
lbaitemple <lbai@temple.edu>
2021-09-10 12:13:48 -0400

    Update stats.py

--- jetbot/apps/stats.py
+++ jetbot/apps/stats.py
@@ -28,11 +28,54 @@ from PIL import ImageFont
 from jetbot.utils.utils import get_ip_address
 
 import subprocess
+import signal
+import time
+
+class GracefulKiller:
+  kill_now = False
+  def __init__(self,display):
+    self.display=display
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.display.clear()
+    self.display.display()
+    self.kill_now = True
 
 # 128x32 display with hardware I2C:
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=None, i2c_bus=1, gpio=1) # setting gpio to 1 is hack to avoid platform detection
+disp.begin()
 
-while True:
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Load default font.
+font = ImageFont.load_default()
+
+while killer.kill_now:
     
     try:
         # Try to connect to the OLED display module via I2C.
