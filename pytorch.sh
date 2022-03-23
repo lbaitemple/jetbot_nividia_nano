f037cb4c3d28472e779a7ae3b3fad74f393a1f20
lbaitemple <lbai@temple.edu>
2022-03-23 10:34:03 -0400

    Update pytorch.sh

--- pytorch.sh
+++ pytorch.sh
@@ -1,5 +1,21 @@
-sudo apt-get install -y gcc python3-dev v4l-utils python3-opencv python3-pip python3-setuptools libffi-dev libssl-dev
-wget https://mathinf.com/pytorch/arm64/torch-1.6.0a0+b31f58d-cp37-cp37m-linux_aarch64.whl
-wget https://mathinf.com/pytorch/arm64/torchvision-0.7.0a0+78ed10c-cp37-cp37m-linux_aarch64.whl
-sudo apt-get install -y python3-numpy python3-wheel python3-setuptools python3-future python3-yaml python3-six python3-requests python3-pip python3-pillow
-pip3 install torch*.whl torchvision*.whl
+#!/bin/bash
+# Animesh Bala Ani (ANI717)
+
+# exit immediately if a command exits with a non-zero status.
+set -e
+
+# record script start time
+date
+
+# install dependency
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-setuptools libopenblas-base libopenmpi-dev
+sudo -H pip3 install --upgrade pip testresources protobuf
+sudo -H pip3 install numpy==1.19.4
+
+# install torch and torchvision
+wget https://nvidia.box.com/shared/static/h1z9sw4bb1ybi0rm3tu8qdj8hs05ljbm.whl -O torch-1.9.0-cp36-cp36m-linux_aarch64.whl
+sudo -H pip3 install torch-1.9.0-cp36-cp36m-linux_aarch64.whl torchvision
+
+# record script end time
+date
