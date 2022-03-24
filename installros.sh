c8ba4ca770aaea705fd455c9ff3220052fe95a9d
lbaitemple <lbai@temple.edu>
2022-03-24 12:39:48 -0400

    Update installros.sh

--- installros.sh
+++ installros.sh
@@ -1,7 +1,8 @@
   
 #!/bin/sh
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu bionic main" > /etc/apt/sources.list.d/ros-melodic.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 sudo apt install ros-melodic-desktop-full -y
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
