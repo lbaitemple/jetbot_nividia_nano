abc3cee873b25ad685178f2e6236e2d2851f0907
lbaitemple <lbai@temple.edu>
2022-03-24 12:50:47 -0400

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -1,37 +1,41 @@
 cd ~
 sudo python3 -m pip install git+https://github.com/ipython/traitlets@4.x
+#!/bin/bash
+# Animesh Bala Ani (ANI717)
 
-# install jupter lab
+# exit immediately if a command exits with a non-zero status.
+set -e
+
+# set default password
+password='jetbot'
+
+# record script start time
+date
+
+# install dependency
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-setuptools curl libffi-dev
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install -y  python3-pip nodejs libffi-dev 
-sudo pip3 install setuptools
-sudo apt install libffi-dev -y
-sudo pip3 install cffi
+sudo apt-get install -y nodejs
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install cffi
 
-sudo pip3 install jupyter jupyterlab
-sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager
-sudo jupyter labextension install @jupyterlab/statusbar
+# install jupyterlab
+sudo -H pip3 install jupyter jupyterlab
+sudo -H jupyter labextension install @jupyter-widgets/jupyterlab-manager
 jupyter lab --generate-config
-python3 -c "from jupyter_server.auth.security import set_password; set_password('jetbot', '$HOME/.jupyter/jupyter_server_config.json')"
-#sudo chown -R pi:pi /home/pi/.local/share/
+python3 -c "from notebook.auth.security import set_password; set_password('$password', '$HOME/.jupyter/jupyter_notebook_config.json')"
 
-# Install bokeh
-#sudo pip3 install bokeh
-#sudo jupyter labextension install @bokeh/jupyter_bokeh
+# set configuration
 echo "c.NotebookApp.token = ''" >> $HOME/.jupyter/jupyter_lab_config.py
 echo "c.NotebookApp.password_required = True" >> $HOME/.jupyter/jupyter_lab_config.py
 echo "c.NotebookApp.allow_credentials = False" >> $HOME/.jupyter/jupyter_lab_config.py
 
-cd ~/jetbot/jetbot/utils
-python3 create_jupyter_service.py
+# create jupyter service
+python3 ./jetbot/utils/create_jupyter_service.py
 sudo mv jetbot_jupyter.service /etc/systemd/system/jetbot_jupyter.service
 sudo systemctl enable jetbot_jupyter
 sudo systemctl start jetbot_jupyter
 
-cd ~/jetbot/jetbot/utils
-python3 create_stats_service.py
-sudo mv jetbot_stats.service /etc/systemd/system/jetbot_stats.service
-sudo systemctl enable jetbot_stats
-sudo systemctl start jetbot_stats
-
-cp -r ~/jetbot/notebooks ~/Notebook
+# record script end time
+date
