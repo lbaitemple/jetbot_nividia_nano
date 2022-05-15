43f3dc6f6ac6f7b309e2635ef90a5b01b1204b8c
lbaitemple <lbai@temple.edu>
2022-05-15 08:25:40 -0400

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -4,6 +4,8 @@ sudo python3 -m pip install git+https://github.com/ipython/traitlets@4.x
 sudo apt install python3-pip python3-pil -y
 
 sudo usermod -aG i2c $USER
+sudo usermod -aG video jetbot
+
 
 bash ./pytorch.sh
 bash ./jetbot_stats.sh
