b148456ee4ff8510c0c1aacaaf106ce5021db638
lbaitemple <lbai@temple.edu>
2022-05-18 16:13:38 -0400

    Update install_ros.sh

--- install_ros.sh
+++ install_ros.sh
@@ -5,7 +5,7 @@ sudo apt install curl  -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt update
-sudo apt install sudo apt install ros-melodic-desktop -y
+sudo apt install ros-melodic-desktop -y
 sudo apt-get install python3 python3-dev python3-pip python3-yaml -y
 sudo pip3 install catkin_pkg empy rospkg
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
