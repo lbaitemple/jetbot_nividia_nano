8ce5bddade64b368d09e88e97bcf0cf0d3207bac
lbaitemple <lbai@temple.edu>
2022-05-18 16:38:29 -0400

    Update install_ros.sh

--- install_ros.sh
+++ install_ros.sh
@@ -8,6 +8,8 @@ sudo apt update
 sudo apt install ros-melodic-desktop -y
 sudo apt-get install python3 python3-dev python3-pip python3-yaml -y
 sudo pip3 install catkin_pkg empy rospkg
+sudo apt install python3-pip python3-all-dev python3-rospkg -y
+
 sudo apt install ros-melodic-desktop-full --fix-missing
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 echo "export ROS_PYTHON_VERSION=3" >> ~/.bashrc
