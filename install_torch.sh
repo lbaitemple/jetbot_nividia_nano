e257947e7e3cec265183bf70fd7281f6ada0dad8
lbaitemple <lbai@temple.edu>
2022-05-27 19:32:56 -0400

    Update install_torch.sh

--- install_torch.sh
+++ install_torch.sh
@@ -1,3 +1,5 @@
+sudo apt-get install libzmq3-dev  python3-smbus curl cmake -y
+sudo apt install python3-pip python3-pil -y
 sudo apt-get install python3-pip libjpeg-dev libopenblas-dev libopenmpi-dev libomp-dev -y
 sudo -H pip3 install future
 sudo pip3 install -U --user wheel mock pillow
