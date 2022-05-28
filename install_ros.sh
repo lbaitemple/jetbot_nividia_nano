7575a7f96d898566602a0a395cfe39bffe8e0dd2
lbaitemple <lbai@temple.edu>
2022-05-27 21:46:24 -0400

    Update install_ros.sh

--- install_ros.sh
+++ install_ros.sh
@@ -19,3 +19,13 @@ sudo apt update
 sudo apt install python3-colcon-common-extensions -y
 sudo pip3 install imageio
 
+## update cmake
+sudo apt install -y software-properties-common lsb-release 
+sudo apt clean all
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+sudo apt update
+sudo apt install kitware-archive-keyring
+sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
+sudo apt update
+sudo apt install cmake -y
