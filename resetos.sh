1fb472230f55e6a08097f08084db7c382fdee00a
lbaitemple <lbai@temple.edu>
2022-05-28 19:48:34 -0400

    Create resetos.sh

---
+++ resetos.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade -y
+sudo mv /var/lib/dpkg/info/ /var/lib/dpkg/backup/
+sudo mkdir /var/lib/dpkg/info/
+sudo apt-get update
+sudo apt-get -f install
+sudo mv /var/lib/dpkg/info/* /var/lib/dpkg/backup/
+sudo rm -rf /var/lib/dpkg/info
+sudo mv /var/lib/dpkg/backup/ /var/lib/dpkg/info/
+sudo apt autoremove -y
