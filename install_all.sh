dae121145be37667b9975c06029ab10b39f3cbe7
lbaitemple <lbai@temple.edu>
2022-05-28 18:13:24 -0400

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,14 +1,14 @@
-sudo apt update && sudo apt upgrade -y
+#sudo apt update && sudo apt upgrade -y
 sudo apt-get install libzmq3-dev  python3-smbus curl cmake -y
-sudo python3 -m pip install git+https://github.com/ipython/traitlets@4.x
+#sudo python3 -m pip install git+https://github.com/ipython/traitlets@4.x
 sudo apt install python3-pip python3-pil -y
 
 sudo usermod -aG i2c $USER
 sudo usermod -aG video jetbot
 
 
-bash ./pytorch.sh
+bash ./install_torch.sh
 bash ./jetbot_stats.sh
 bash ./jupyter.sh
-bash ./installros.sh
-bahs ./install_d435.sh
+bash ./install_ros.sh
+#bahs ./install_d435.sh
