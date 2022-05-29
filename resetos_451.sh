1c64fb32e9a7faa016f3ee60b1d09d73c6a9a948
lbaitemple <lbai@temple.edu>
2022-05-29 08:03:54 -0400

    Update and rename resetos.sh to resetos_451.sh

--- resetos_451.sh
+++ resetos_451.sh
@@ -1,3 +1,8 @@
+### This code will update the OS for image Jetpack 451 or Jetpack 45
+### it seems to have the dpkg: error "processing package nvidia-l4t-bootloader (--configure):"
+### run this code will update the system for 451 and 45
+
+
 sudo apt update && sudo apt upgrade -y
 sudo mv /var/lib/dpkg/info/ /var/lib/dpkg/backup/
 sudo mkdir /var/lib/dpkg/info/
