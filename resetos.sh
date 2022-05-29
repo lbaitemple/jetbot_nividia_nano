9cf41060ee5f24fedc8e06948fe3223e9e2886c6
lbaitemple <lbai@temple.edu>
2022-05-28 23:20:50 -0400

    Update resetos.sh

--- resetos.sh
+++ resetos.sh
@@ -2,7 +2,7 @@ sudo apt update && sudo apt upgrade -y
 sudo mv /var/lib/dpkg/info/ /var/lib/dpkg/backup/
 sudo mkdir /var/lib/dpkg/info/
 sudo apt-get update
-sudo apt-get -f install
+sudo apt-get -f -y install
 sudo mv /var/lib/dpkg/info/* /var/lib/dpkg/backup/
 sudo rm -rf /var/lib/dpkg/info
 sudo mv /var/lib/dpkg/backup/ /var/lib/dpkg/info/
