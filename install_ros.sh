a9f35945133dd0ae2a0c1fc35bd18f6de44d4acb
lbaitemple <lbai@temple.edu>
2022-05-29 17:13:20 -0400

    Update install_ros.sh

--- install_ros.sh
+++ install_ros.sh
@@ -13,9 +13,6 @@ sudo apt install python3-pip python3-all-dev python3-rospkg python3-catkin-pkg -
 
 
 sudo apt install -y ros-melodic-desktop --fix-missing
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-echo "export ROS_PYTHON_VERSION=3" >> ~/.bashrc
-source ~/.bashrc
 #sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros-latest.list'
 #sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
@@ -26,6 +23,9 @@ sudo apt-get install python3-pip
 sudo pip3 install -U rosdep
 sudo rosdep init
 rosdep update
+echo "export ROS_PYTHON_VERSION=3" >> ~/.bashrc
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
  
  
 ## update cmake
