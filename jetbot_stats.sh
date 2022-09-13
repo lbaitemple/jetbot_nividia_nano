f8f572043a15f1b3f93f63909b275f976771f193
lbaitemple <lbai@temple.edu>
2022-09-12 21:58:49 -0400

    Update jetbot_stats.sh

--- jetbot_stats.sh
+++ jetbot_stats.sh
@@ -15,7 +15,9 @@ cd ~/jetbot
 sudo python3 setup.py install
 sudo pip3 install packaging ipywidgets
 sudo -H pip3 install --upgrade pip
-sudo -H pip3 install Adafruit_SSD1306
+#sudo -H pip3 install Adafruit_SSD1306
+sudo python3 -m pip install Adafruit_SSD1306
+sudo python3 -m pip install opencv-python
 
 # create jetbot_stats service
 python3 ./jetbot/utils/create_stats_service.py
