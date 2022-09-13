2cc5a16d4744af3e7c486e211fd243dc319bcc4d
lbaitemple <lbai@temple.edu>
2022-09-13 10:24:29 -0400

    Create install_torch.sh

---
+++ install_torch.sh
@@ -0,0 +1,26 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install libzmq3-dev  python3-smbus curl cmake -y
+sudo apt install python3-pip python3-pil -y
+sudo apt-get install python3-pip libjpeg-dev libopenblas-dev libopenmpi-dev libomp-dev -y
+sudo -H pip3 install future
+sudo pip3 install -U --user wheel mock pillow
+sudo -H pip3 install testresources 
+sudo pip3 install packaging
+# above 58.3.0 you get version issues
+sudo -H pip3 install setuptools==58.3.0
+sudo -H pip3 install Cython
+# install gdown to download from Google drive
+sudo -H pip3 install gdown
+# download the wheel
+gdown https://drive.google.com/uc?id=1TqC6_2cwqiYacjoLhLgrZoap6-sVL2sd
+# install PyTorch 1.10.0
+sudo -H pip3 install torch-1.10.0a0+git36449ea-cp36-cp36m-linux_aarch64.whl
+# clean up
+rm torch-1.10.0a0+git36449ea-cp36-cp36m-linux_aarch64.whl
+sudo -H pip3 install torchvision
+
+### torch2rt
+cd ~
+git clone https://github.com/NVIDIA-AI-IOT/torch2trt
+cd ~/torch2trt
+sudo python3 setup.py install
